ee4c7f8fbec9aedfbf96518aab40b376387f70ce
Yash Lahase <yashlahase1234@gmail.com>
2026-01-29 21:41:58 +0530

    configure axios API client

---
+++ frontend/src/api/axios.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+
+const API = axios.create({
+    baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+});
+
+// Request interceptor to add auth token
+API.interceptors.request.use(
+    (config) => {
+        const token = localStorage.getItem('token');
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
