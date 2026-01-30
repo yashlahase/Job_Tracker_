61a9fe30de3bd039a9d7aa766877032575a6597f
Yash Lahase <yashlahase1234@gmail.com>
2026-01-30 16:41:28 +0530

    filter component added

---
+++ frontend/src/components/Filters.jsx
@@ -0,0 +1,6 @@
+const Filters = ({ filters, onFilterChange }) => {
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        onFilterChange({ ...filters, [name]: value });
+    };
+}
